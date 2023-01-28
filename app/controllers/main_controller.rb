e6fff330551057b7ec0a6760c30f7b752dfb9c3c
Vladboy2 <vlad02boyko@gmail.com>
2023-01-27 19:03:24 -0500

    Added flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
     def index
+        #feature/method inherited from ApplicationController to notify the user what happened as a result of their actions 
+        flash[:notice] = "Logged in successfully" #use it like a flash
+        flash[:alert] = "Invalid email or password"
     end
 end
