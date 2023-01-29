483b2af98ea38f120d8670ecc58643012da1ecbc
Vladboy2 <vlad02boyko@gmail.com>
2023-01-29 14:40:51 -0500

    user log out

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,10 @@
 class MainController < ApplicationController
     def index
         #feature/method inherited from ApplicationController to notify the user what happened as a result of their actions 
-        flash[:notice] = "Logged in successfully" #use it like a flash
-        flash[:alert] = "Invalid email or password"
+        # flash[:notice] = "Logged in successfully" #use it like a flash
+        # flash[:alert] = "Invalid email or password"
+        if session[:user_id]
+            @user = User.find_by(id: session[:user_id]) # find_by will not throw an error when it cant find a user
+        end
     end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,33 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        render plain: params[:users]
+        
+        @user = User.new(user_params) # user_params is a helper method that is private and makes it more secure
+
+        if @user.save
+            #set cookie to keep track of the user signed in
+            #cookies[:user_id] <--code
+
+            #Two types of cookies: session and signed
+            #session is an encrypted store
+            session[:user_id] = @user.id
+            
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            render :new
+        end 
+
+        #params # => {"authenticity_token"=>"[FILTERED]", "user"=>{"email"=>"vlad02boyko@gmail.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"Sign Up"}
+
+        #params[:user] # => {"email"=>"vlad02boyko@gmail.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"Sign Up"}
+        
+        #render plain: params[:user]
     end
     
+    private 
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
 end
