5101fad9e452483b4964d59da2fb9af906a0f9ca
Vladboy2 <vlad02boyko@gmail.com>
2023-01-29 19:22:37 -0500

    user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+    
+    def new
+
+    end
+
+    def create
+        user = User.find_by(email: params[:email]) # find_by method takes in hash param
+
+        if user.present? && user.authenticate(params[:password]) # look up the user and valid password. authenticate takes in string param
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            flash[:alert] = "Invalid email or password"
+            render :new
+        end
+
+    end
+    
     def destory
         session[:user_id]= nil
         redirect_to root_path, notice: "logged out"
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,19 @@
   # root "articles#index"
 Rails.application.routes.draw do
   
+  #get is for the form
+  #post is for the form submission
+
   # GET /about (Respond to GET request) 
   # Points to controller for about
   get "about-us", to: "about#index", as: :about #Changing the url but leave the name the same
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destory"
 
   # root to main page
