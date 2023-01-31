9764b2db26db8d155676dd3f5283c53fb2933cf9
Vladboy2 <vlad02boyko@gmail.com>
2023-01-30 19:11:48 -0500

    password forgot/reset

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   # Points to controller for about
   get "about-us", to: "about#index", as: :about #Changing the url but leave the name the same
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
@@ -18,6 +21,12 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destory"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 
   # root to main page
   # custom helper : 
